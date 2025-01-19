3580e23c260508a6bc4bdf268ef05acc466884eb
bf4r <sharpslpcz@gmail.com>
2025-01-19 16:34:22 +0100

    Add save mode, new commands, commands from text mode

--- Editor.cs
+++ Editor.cs
@@ -8,43 +8,61 @@ public class Editor
             Read();
         }
     }
-    // text, command
+    // command, text, save
     public string Mode = "command";
     public Dictionary<string, string> Buffers = new();
     public string? CurrentBuffer;
     public void Read()
     {
-        if (CurrentBuffer == null)
-        {
-            Mode = "command";
-        }
+        if (CurrentBuffer == null) Mode = "command";
+
         if (Mode == "command")
         {
             Console.WriteLine("C");
-            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
-            ConsoleKey key = keyInfo.Key;
-            switch (key)
+            var input = Console.ReadLine();
+            if (input == "t")
             {
-                case ConsoleKey.N:
-                    Mode = "text";
-                    string newBufferName = Guid.NewGuid().ToString();
-                    Buffers.Add(newBufferName, "");
-                    CurrentBuffer = newBufferName;
-                    break;
+                Mode = "text";
+                string newBufferName = Guid.NewGuid().ToString();
+                Buffers.Add(newBufferName, "");
+                CurrentBuffer = newBufferName;
             }
         }
         else if (Mode == "text")
         {
+            Console.Write("T");
             string input = Console.ReadLine() ?? "";
             if (input.StartsWith(@"\"))
             {
                 input = input.Substring(1);
             }
-            if (input == "@C")
+
+            if (input == "@!c")
             {
                 Mode = "command";
             }
-            Buffers[CurrentBuffer!] += Environment.NewLine + input;
+            else if (input == "@!s")
+            {
+                Mode = "save";
+            }
+            // Buffer ID
+            else if (input == "@/bi")
+            {
+                Console.WriteLine(CurrentBuffer);
+            }
+            else
+            {
+                Buffers[CurrentBuffer!] += input + Environment.NewLine;
+            }
+        }
+        else if (Mode == "save")
+        {
+            Console.WriteLine("S");
+            string fileName = Console.ReadLine() ?? Guid.NewGuid().ToString() + ".txt";
+            string workingDirectory = Directory.GetCurrentDirectory();
+            string fullPath = Path.Combine(workingDirectory, fileName);
+            string bufferText = Buffers[CurrentBuffer!];
+            File.WriteAllText(fullPath, bufferText);
         }
     }
 }
